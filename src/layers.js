81926930dc91e0abfa691ea994411280f7dd9041
Greg Lauer <50904251+MutantKiwi@users.noreply.github.com>
2026-04-13 21:19:48 +1000

    Add referrerPolicy to layer configuration

--- src/layers.js
+++ src/layers.js
@@ -33,6 +33,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
                             print: true,
                             jnx: true,
                             shortName: 'osm',
+                            referrerPolicy: 'strict-origin', 
                             attribution: '<a href="https://www.openstreetmap.org/copyright">' +
                                 '&copy; OpenStreetMap contributors</a>',
                         }
